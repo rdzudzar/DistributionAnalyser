c1f338ce6893503731243bbfa8f90218529fb179
Robert Dzudzar <33647339+rdzudzar@users.noreply.github.com>
2022-10-20 16:39:36 +1100

    Bokeh

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main():
     st.sidebar.warning(
             """
             Distribution Analyser app is created and maintained by astrophysicist
-            **Dr Robert Dzudzar**, currently Executive Data Scientist at [**Tribalism**](https://www.tribalism.com.au/?utm_source=WebApp&utm_medium=DistributionAnalyser). 
+            **Dr Robert Dzudzar**. 
             If you like this app please star its
             [**GitHub**](https://github.com/rdzudzar/DistributionAnalyser)
             repo, share it and feel free to open an issue if you find a bug 
